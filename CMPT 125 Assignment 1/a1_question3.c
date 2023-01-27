c32596bf3e220370419bd1c41a5e3372729064ba
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-01-26 18:27:14 -0800

    Update a1_question3.c

---
+++ CMPT 125 Assignment 1/a1_question3.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "a1_question3.c"
+
+float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
+    // initialize variables with zero
+    float s1 = 0;
+    float s2 = 0;
+    float area = 0;
+
+    for (int i=0; i<; i++) {
+        s1 = points[i][2]*points[i+1][1];
+        s2 = points[i][1]*points[i-1][2];
+        
+
+
+    }
+
+
+}
