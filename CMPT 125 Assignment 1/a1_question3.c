8ca7d95f2834abb52dde0ac95d6352c505c3cf3b
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-01-27 17:20:24 -0800

    Update a1_question3.c

--- CMPT 125 Assignment 1/a1_question3.c
+++ CMPT 125 Assignment 1/a1_question3.c
@@ -5,8 +5,8 @@
 
 float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
     // initialize variables with zero
-    int s1 = 0;
-    int s2 = 0;
+    float s1 = 0;
+    float s2 = 0;
     float area = 0;
     int indexmatch = 0;
 
@@ -27,9 +27,8 @@ float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
                     s1 += (points[i][0]*points[0][1]);
                 }  
             }
-
             if (i+1 == index){
-                s1 += (points[i][0]*points[i+2][1]);
+                s1 += (points[i][0]*points[i][1]);
             }
             //
             else {
@@ -51,26 +50,17 @@ float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
                 }  
             }
             if (i+1 == index){
-                s2 += (points[i][1]*points[i+2][0]);
+                s2 += (points[i][1]*points[i][0]);
             }
             //
             else {
                 s2 += (points[i][1]*points[i+1][0]);
             }
+        }     
         }
-              
-        }
-
         area = 0.5* fabs(s1-s2);
         return area;
     }
-
-int main () {
-    printf(calculateAreaShoelace(4, matrix1, 1));
-}
     
     
     
-
-
- 
