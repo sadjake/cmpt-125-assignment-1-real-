841a8daf1a0bd6be6674b616cecc3129459fb7d9
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-01-27 16:28:34 -0800

    Update a1_question3.c

--- CMPT 125 Assignment 1/a1_question3.c
+++ CMPT 125 Assignment 1/a1_question3.c
@@ -1,21 +1,76 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "a1_question3.c"
+#include "a1_question3.h"
 
 float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
     // initialize variables with zero
-    float s1 = 0;
-    float s2 = 0;
+    int s1 = 0;
+    int s2 = 0;
     float area = 0;
+    int indexmatch = 0;
 
-    for (int i=0; i<; i++) {
-        s1 = points[i][2]*points[i+1][1];
-        s2 = points[i][1]*points[i-1][2];
-        
+    // for loop
+    for (int i=0; i<rows; i++) {
+        // set conditions to multiply x and ys
+        if (i==index) { 
+            indexmatch += 1;
+            }
 
+        else {
+            if (i==rows-1) {
+                if(index == 0) {
+                    s1 += (points[i][0]*points[1][1]);
+                }
 
+                else {
+                    s1 += (points[i][0]*points[0][1]);
+                }  
+            }
+
+            if (i+1 == index){
+                s1 += (points[i][0]*points[i+2][1]);
+            }
+            //
+            else {
+                s1 += (points[i][0]*points[i+1][1]); 
+            }
+        }      
+    }
+        for (int i=0; i<rows; i++) {
+        // 
+        if (i==index) { 
+            indexmatch += 1;
+            }
+        else {
+            if (i==rows-1) {
+                if(index == 0){
+                    s2 += (points[i][1]*points[1][0]);
+                }else{
+                    s2 += (points[i][1]*points[0][0]);
+                }  
+            }
+            if (i+1 == index){
+                s2 += (points[i][1]*points[i+2][0]);
+            }
+            //
+            else {
+                s2 += (points[i][1]*points[i+1][0]);
+            }
+        }
+              
+        }
+
+        area = 0.5* fabs(s1-s2);
+        return area;
     }
 
-
+int main () {
+    printf(calculateAreaShoelace(4, matrix1, 1));
 }
+    
+    
+    
+
+
+ 
