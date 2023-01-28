af48df59014df8d902c0508aa9ca38defdc86664
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-01-27 22:12:37 -0800

    Create a1_question3v2.c

---
+++ CMPT 125 Assignment 1/a1_question3v2.c
@@ -0,0 +1,24 @@
+#include "a1_question3.h"
+
+float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
+    // initialize variables with zero
+    float s1 = 0;
+    float s2 = 0;
+    float area = 0;
+
+    // make a variable to store values to be re-used
+    int storage1;
+    int storage2;
+
+    // 
+
+    //
+    if (int i=0; i<rows; i++) {
+        
+
+    }
+
+}
+
+
+
