67e1f75a22a41a46efe4831ac359f2539fe71898
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-01-28 21:14:08 -0800

    Update a1_question3.c

--- CMPT 125 Assignment 1/a1_question3.c
+++ CMPT 125 Assignment 1/a1_question3.c
@@ -6,26 +6,42 @@
 
 float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
     // initialize s1, s2 with zero
-    int s1 = 0;
-    int s2 = 0;
-    // initialize area to zero
-    int area = 0;
+    float s1 = 0;
+    float s2 = 0;
+    // initialize final answer variable (area) to zero
+    float area = 0;
 
-    int zero1;
-    int zero2;
+    // create variables for values in removed rows
+    float zero1;
+    float zero2;
 
-    // set 0 to values for values in removed rows
-    if (0<=index<rows) {
+    if (index<rows) { 
         zero1 = points[index][0];
         zero2 = points[index][1];
+        
+        // set the values to 0
         points[index][0] = 0;
         points[index][1] = 0;
     }
 
+    // variables to multiply the x and y values
+    int mult1;
+    int mult2;
+
     for (int i=0; i<rows-1; i++) {
         // set conditions to multiply x and y values
-        s1 += (points[i][0]*points[i+1][1]);
-        s2 += (points[i][1]*points[i+1][0]);
+
+        // multiplying x with a y that is 1 greater
+        // than x's position in the columns
+        mult1 = (points[i][0]*points[i+1][1]);
+
+        // multiplying x with a y that is 1 lesser
+        // than x's position in the columns
+        mult2 = (points[i][1]*points[i+1][0]);
+
+        // add them to s1 and s2
+        s1 += mult1;
+        s2 += mult2;
     }
         
     // multiply the last and first points
@@ -40,6 +56,7 @@ float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
     s1 += lastm1;
     s2 += lastm2;
 
+    // calculation for area
     area += 0.5*(fabs(s1-s2));
 
     points[index][0] = zero1;
@@ -48,3 +65,6 @@ float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
     // end result (area)
     return area;
     }
+
+
+    
