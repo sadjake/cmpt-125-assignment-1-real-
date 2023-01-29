bcdba71bfd9f50e22d1b4bc2cf621b62bd01c1d3
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-01-28 20:36:43 -0800

    Update a1_question3.c

--- CMPT 125 Assignment 1/a1_question3.c
+++ CMPT 125 Assignment 1/a1_question3.c
@@ -5,66 +5,46 @@
 #include "a1_question3.h"
 
 float calculateAreaShoelace(unsigned int rows, int points[][2], int index) {
-    // initialize variables with zero
-    float s1 = 0;
-    float s2 = 0;
-    float area = 0;
-    int indexmatch = 0;
+    // initialize s1, s2 with zero
+    int s1 = 0;
+    int s2 = 0;
+    // initialize area to zero
+    int area = 0;
 
-    // for loop
-    for (int i=0; i<rows; i++) {
-        // set conditions to multiply x and ys in shoelace way
-        if (i==index) { 
-            indexmatch += 1;
-            }
+    int zero1;
+    int zero2;
 
-        else {
-            // row-1
-            if (i==rows-1) {
-                if(index == 0) {
-                    // x1 times y2
-                    s1 += (points[i][0]*points[1][1]);
-                }
-
-                else {
-                    s1 += (points[i][0]*points[0][1]);
-                }  
-            }
-            if (i+1 == index){
-                s1 += (points[i][0]*points[i][1]);
-            }
-            //
-            else {
-                s1 += (points[i][0]*points[i+1][1]); 
-            }
-        }      
+    // set 0 to values for values in removed rows
+    if (0<=index<rows) {
+        zero1 = points[index][0];
+        zero2 = points[index][1];
+        points[index][0] = 0;
+        points[index][1] = 0;
     }
-        for (int i=0; i<rows; i++) {
-        // set conditions to multiply x and ys in shoelace way
-        if (i==index) { 
-            indexmatch += 1;
-            }
-        else {
-            if (i==rows-1) {
-                if(index == 0){
-                    s2 += (points[i][1]*points[1][0]);
-                }else{
-                    s2 += (points[i][1]*points[0][0]);
-                }  
-            }
-            if (i+1 == index){
-                s2 += (points[i][1]*points[i][0]);
-            }
-            // 
-            else {
-                s2 += (points[i][1]*points[i+1][0]);
-            }
-        }     
-        }
-        // calculation of the area
-        area = 0.5* fabs(s1-s2);
-        return area;
+
+    for (int i=0; i<rows-1; i++) {
+        // set conditions to multiply x and y values
+        s1 += (points[i][0]*points[i+1][1]);
+        s2 += (points[i][1]*points[i+1][0]);
     }
-    
-    
-    
+        
+    // multiply the last and first points
+    // of s1 and s2
+    int lastm1;
+    int lastm2; 
+
+    lastm1 = points[rows-1][0]*points[0][1];
+    lastm2 = points[0][0]*points[rows-1][1];
+
+    // add them to s1 and s2
+    s1 += lastm1;
+    s2 += lastm2;
+
+    area += 0.5*(fabs(s1-s2));
+
+    points[index][0] = zero1;
+    points[index][1] = zero2;
+
+    // end result (area)
+    return area;
+    }
